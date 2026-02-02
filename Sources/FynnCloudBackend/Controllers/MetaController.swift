7e1b714d9895289f71e5464cd5cc45da26c66273
Thilo <thilo0123@gmail.com>
2026-02-02 01:46:29 +0100

    Forward app config cloud name and theme color to frontend

--- Sources/FynnCloudBackend/Controllers/MetaController.swift
+++ Sources/FynnCloudBackend/Controllers/MetaController.swift
@@ -1,9 +1,11 @@
 import Vapor
 
 struct ServerInfo: Content {
+    let appName: String
     let version: String
     let maxFileSize: Int64
     let environment: String
+    let primaryColor: String
 
 }
 struct MetaController: RouteCollection {
@@ -15,9 +17,44 @@ struct MetaController: RouteCollection {
 
     func info(req: Request) async throws -> ServerInfo {
         return ServerInfo(
+            appName: Environment.process.APP_NAME ?? "FynnCloud",
             version: "1.0.0",
             maxFileSize: Int64(req.application.routes.defaultMaxBodySize.value),
             environment: req.application.environment.name,
+            primaryColor: Self.getColor(),
         )
+    }
+
+    private static func getColor() -> String {
+        let allowedColors = [
+            "slate",
+            "gray",
+            "zinc",
+            "neutral",
+            "stone",
+            "red",
+            "orange",
+            "amber",
+            "yellow",
+            "lime",
+            "green",
+            "emerald",
+            "teal",
+            "cyan",
+            "sky",
+            "blue",
+            "indigo",
+            "violet",
+            "purple",
+            "fuchsia",
+            "pink",
+            "rose",
+        ]
+
+        if let envColor = Environment.process.PRIMARY_COLOR, allowedColors.contains(envColor) {
+            return envColor
+        }
+
+        return "blue"
     }
 }
