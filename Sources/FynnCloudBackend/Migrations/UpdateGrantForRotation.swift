083e6eeda73656a40768efdd2707b0ee6b7e7fa3
Thilo <thilo0123@gmail.com>
2026-02-02 01:46:56 +0100

    update to use swift conventions

--- Sources/FynnCloudBackend/Migrations/UpdateGrantForRotation.swift
+++ Sources/FynnCloudBackend/Migrations/UpdateGrantForRotation.swift
@@ -3,11 +3,11 @@ import Fluent
 struct UpdateGrantForRotation: AsyncMigration {
     func prepare(on database: any Database) async throws {
         try await database.schema("oauth_grants")
-            .field("current_refresh_token_id", .uuid)  // Matches your @Field
+            .field("current_refresh_token_id", .uuid)
             .update()
     }
 
-    func revert(on database: Database) async throws {
+    func revert(on database: any Database) async throws {
         try await database.schema("oauth_grants")
             .deleteField("current_refresh_token_id")
             .update()
