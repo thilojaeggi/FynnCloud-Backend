6426c00cfcaa0cecbdb7072737349cb80452ddff
Thilo <thilo0123@gmail.com>
2026-02-04 04:10:55 +0100

    Add debug routes/dev routes if not release

--- Sources/FynnCloudBackend/routes.swift
+++ Sources/FynnCloudBackend/routes.swift
@@ -7,10 +7,14 @@ func routes(_ app: Application) throws {
     let metaController = MetaController()
     let syncController = SyncController()
     let userController = UserController()
+    let debugController = DebugController()
 
     try app.register(collection: authController)
     try app.register(collection: fileController)
     try app.register(collection: metaController)
     try app.register(collection: syncController)
     try app.register(collection: userController)
+    if !app.environment.isRelease {
+        try app.register(collection: debugController)
+    }
 }
